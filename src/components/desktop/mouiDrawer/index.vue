c432ae3896aed644d46d95568a561ef778e861e2
fahrihub <fahri7745@gmail.com>
2022-08-16 13:36:01 +0700

    drawer modif 16-8

--- src/components/desktop/mouiDrawer/index.vue
+++ src/components/desktop/mouiDrawer/index.vue
@@ -9,21 +9,32 @@
         app
     >
         <template v-slot:prepend>
-            <v-toolbar class="rounded-t-lg" :class="drawer.mini ? 'mini-mode' : ''" flat>
-                <v-avatar v-if="drawer.mini"
-                    :color="theme" 
-                    size="40" 
+            <v-toolbar
+                class="rounded-t-lg"
+                :class="drawer.mini ? 'mini-mode' : ''"
+                flat
+            >
+                <v-avatar
+                    v-if="drawer.mini"
+                    :color="theme"
+                    size="40"
                     @click="drawer.mini = !drawer.mini"
-                    style="cursor: pointer;"
+                    style="cursor: pointer"
                 >
-                    <div class="font-fredoka-one white--text text-center" style="font-size: 22px;">M</div>
+                    <div
+                        class="font-fredoka-one white--text text-center"
+                        style="font-size: 22px"
+                    >
+                        A
+                    </div>
                 </v-avatar>
 
-                <v-toolbar-title v-else
+                <v-toolbar-title
+                    v-else
                     class="text-uppercase pl-0"
                     :class="`${theme}--text`"
                 >
-                    <span class="font-fredoka-one spacing-1">Monoland</span>
+                    <span class="font-fredoka-one spacing-1">AgendaKU</span>
                 </v-toolbar-title>
 
                 <v-spacer></v-spacer>
@@ -45,8 +56,20 @@
         <template v-slot:append>
             <v-sheet class="rounded-b-lg" height="32">
                 <v-scale-transition origin="center center 0">
-                    <div class="d-flex justify-center overline" key="mini-on" v-if="drawer.mini"><small>m&copy;22</small></div>
-                    <div class="d-flex justify-center overline" key="mini-off" v-else><small>monoland.soft&copy;2022</small></div>
+                    <div
+                        class="d-flex justify-center overline"
+                        key="mini-on"
+                        v-if="drawer.mini"
+                    >
+                        <small>m&copy;22</small>
+                    </div>
+                    <div
+                        class="d-flex justify-center overline"
+                        key="mini-off"
+                        v-else
+                    >
+                        <small>monoland.soft&copy;2022</small>
+                    </div>
                 </v-scale-transition>
             </v-sheet>
         </template>
@@ -54,26 +77,27 @@
 </template>
 
 <script>
-import { storeToRefs } from 'pinia';
-import { useSystemStore } from '@stores/systemStore';
+import { storeToRefs } from "pinia";
+import { useSystemStore } from "@stores/systemStore";
 
 export default {
-    name: 'moui-drawer',
+    name: "moui-drawer",
 
     setup() {
         const systemStore = useSystemStore();
         const { drawer, theme } = storeToRefs(systemStore);
 
-        return { 
+        return {
             systemStore,
-            drawer, theme
-         };
+            drawer,
+            theme,
+        };
     },
-}
+};
 </script>
 
 <style>
-    .v-toolbar.mini-mode > .v-toolbar__content {
-        padding: 4px 7px;
-    }
+.v-toolbar.mini-mode > .v-toolbar__content {
+    padding: 4px 7px;
+}
 </style>
